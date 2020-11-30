6235627def67f30a44a13787c6e56be37cfad30b
vflproject <vocalforlocalteam@gmail.com>
2020-11-30 08:49:06 +0530

    eight commit

--- frontend1/taluk/home.js
+++ frontend1/taluk/home.js
@@ -1,7 +1,9 @@
 var tid = 1;
 var list = [];
 var sublist = [];
+var megasublist = []
 var eachlist = [];
+var megaeachsublist = [];
 var crops = [];
 fetch(`http://localhost:8080/taluk/viewcrops/${tid}`)
      .then(response => {
@@ -25,8 +27,20 @@ fetch(`http://localhost:8080/taluk/viewfarmers/${tid}`)
          
          for(var j=0;j<data.length;j++)
          {
-             var temp =  `${data[j].aadharno}   ${data[j].farmername}   ${data[j].accountno}    ${data[j].balance}  ${data[j].paid}  <button onclick="farmerdetail(${data[j].aadharno})">view</button>`
+             var temp =  `${data[j].aadharno}`   
              sublist.push(temp);
+             var temp = `${data[j].farmername}` 
+             sublist.push(temp);  
+             var temp = `${data[j].accountno}`   
+             sublist.push(temp);
+             var temp = `${data[j].balance}`  
+             sublist.push(temp);
+             var temp = `${data[j].paid}`
+             sublist.push(temp);
+             var temp = `<button onclick="farmerdetail(${data[j].aadharno})">view</button>`
+             sublist.push(temp);
+             megasublist.push(sublist);
+             sublist =[];
          }
      })
      .catch((error)=>{
@@ -46,6 +60,21 @@ fetch(`http://localhost:8080/dist/distcrop/onlycrop`)
      .catch((error)=>{
         console.log(error)
      })
+//Global post data function
+async function postData(url = '', data = {})
+  {
+         const response = await fetch(url, {
+             method: 'POST',
+             mode :'cors',
+             cache : 'no-cache',
+             credentials: 'same-origin',
+             headers: {
+                 'Content-Type' : 'application/json'
+             },
+             body: JSON.stringify(data)
+         });
+         return response.json();
+  }
 function viewcrops()
      {
             document.getElementById("view").innerHTML="";
@@ -58,10 +87,18 @@ function viewcrops()
 function viewfarmers()
      {
          document.getElementById("view").innerHTML="";
-         for(var i=0;i<sublist.length;i++)
+         var namelist = "<tr> <th>aadharno</th><th>farmername</th><th>account no</th><th>balance</th><th>paid</th> </tr>";
+         document.getElementById("view").innerHTML += namelist;
+         for(var i=0;i<megasublist.length;i++)
          {
-             var namelist = "<li>" +sublist[i]+  "</li>";
-             document.getElementById("view").innerHTML += namelist;
+            var namelist = "<tr> <td>"+ 
+                megasublist[i][0] +"</td><td>"+ 
+                megasublist[i][1] +"</td><td>"+
+                megasublist[i][2] +"</td><td>"+ 
+                megasublist[i][3] +"</td><td>"+ 
+                megasublist[i][4] +"</td><td>"+ 
+                megasublist[i][5] +"</td></tr>";
+            document.getElementById("view").innerHTML += namelist;
          }
      }
 function addbalance(ano)
@@ -85,7 +122,6 @@ function addbalancevalue()
          return response.json();
      })
      .then(data => {
-        console.log("success")
         document.getElementById("second").innerHTML=""
         farmerdetail(ano);
      })
@@ -109,9 +145,26 @@ function payment(ano)
         console.log(error)
      })
 }
-function editfarmer()
-{
 
+function editfarmer(ano,name,accno)
+{
+    document.getElementById("ad").value = ano;
+    document.getElementById("fn").value = name;
+    document.getElementById("ac").value = accno;
+    document.getElementById("edit").style.display = "block"
+}
+function editing()
+{
+    var ad = document.getElementById("ad").value
+    var fn = document.getElementById("fn").value
+    var ac = document.getElementById("ac").value
+    postData(`http://localhost:8080/taluk/editfarmer/${tid}/${ad}`, {farmername:fn,accountno:ac})
+    .then(data => {
+        console.log("success")
+    })
+    .catch((error)=>{
+        console.log(error);
+    })
 }
 function farmerdetail(id)
 {
@@ -122,18 +175,33 @@ function farmerdetail(id)
          return res.json();
      })
      .then(data=>{
-        var temp = `aadharno  farmername  accountno  balance  paid`;
-        var namelist = "<li>" +temp+  "</li>";
         for(var j=0;j<data.length;j++)
          {
-             var temp =  `${data[j].aadharno}   ${data[j].farmername}   ${data[j].accountno}  ${data[j].balance} ${data[j].paid}`
-             var namelist = "<li>" +temp+  "</li>";
-             document.getElementById("second").innerHTML += namelist;
+            var temp =  `${data[j].aadharno}`   
+            eachlist.push(temp);
+            var temp = `${data[j].farmername}` 
+            eachlist.push(temp);  
+            var temp = `${data[j].accountno}`   
+            eachlist.push(temp);
+            var temp = `${data[j].balance}`  
+            eachlist.push(temp);
+            var temp = `${data[j].paid}`
+            eachlist.push(temp);
+            var namelist = "<tr> <th>aadharno</th><th>farmername</th><th>account no</th><th>balance</th><th>paid</th> </tr>";
+            document.getElementById("second").innerHTML += namelist;
+            var namelist = "<tr> <td>"+ 
+            eachlist[0] +"</td><td>"+ 
+            eachlist[1] +"</td><td>"+
+            eachlist[2] +"</td><td>"+ 
+            eachlist[3] +"</td><td>"+ 
+            eachlist[4] +"</td></tr>";
+        document.getElementById("second").innerHTML += namelist;
+
              var namelist = `<button onclick="addbalance(${data[j].aadharno})">add balance</button>`;
              document.getElementById("second").innerHTML += namelist;
              var namelist = `<button onclick="payment(${data[j].aadharno})">PAY</button>`;
              document.getElementById("second").innerHTML += namelist;
-             var namelist = `<button onclick="editfarmer(${data[j].aadharno})">EDIT</button>`;
+             var namelist = `<button onclick="editfarmer(${data[j].aadharno},'${data[j].farmername}',${data[j].accountno})">EDIT</button>`;
              document.getElementById("second").innerHTML += namelist;
          }
      })
